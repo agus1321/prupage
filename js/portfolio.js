a46d94808b91772fe54b5904e5add46ad58c668c
Agustin Fiasche <30364925+agus1321@users.noreply.github.com>
2022-10-26 22:22:42 -0300

    V2 Final

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,47 @@
-window.fn = {};
+function mostoFias(){
+    document.getElementById('af').style.display = 'block';
+    document.getElementById('bf').style.display = 'none';
+    document.getElementById('ds').style.display = 'none';
+}
+function mostoBri(){
+    document.getElementById('bf').style.display = 'block';
+    document.getElementById('af').style.display = 'none';
+    document.getElementById('ds').style.display = 'none';
+}
+function mostoSol(){
+    document.getElementById('ds').style.display = 'block';
+    document.getElementById('af').style.display = 'none';
+    document.getElementById('bf').style.display = 'none';
+}
 
-window.fn.toggleMenu = function() {
-    document.getElementById('appSplitter').right.toggle();
-};
 
-window.fn.loadView = function(index) {
-    document.getElementById('appTabbar').setActiveTab(index);
-    document.getElementById('sidemenu').close();
-};
+/*----------------activacion de botones-----------------------*/
 
-window.fn.loadLink = function(url) {
-    window.open(url, '_blank');
-};
 
-window.fn.pushPage = function(page, anim) {
-    if (anim) {
-        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
-    } else {
-        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
-    }
-};
+
+function activarButtFias(){
+    document.getElementById('imgFias').style.boxShadow = '0px 0px 10px var(--btnPort)';
+    document.getElementById('imgFias').style.opacity = '1';
+    document.getElementById('imgBri').style.opacity = '0.4';
+    document.getElementById('imgBri').style.boxShadow = 'none';
+    document.getElementById('imgSol').style.opacity = '0.4';
+    document.getElementById('imgSol').style.boxShadow = 'none';
+}
+
+function activarButtBri(){
+    document.getElementById('imgBri').style.boxShadow = '0px 0px 10px var(--btnPort)';
+    document.getElementById('imgBri').style.opacity = '1';
+    document.getElementById('imgSol').style.opacity = '0.4';
+    document.getElementById('imgSol').style.boxShadow = 'none';
+    document.getElementById('imgFias').style.opacity = '0.4';
+    document.getElementById('imgFias').style.boxShadow = 'none';
+}
+
+function activarButtSol(){
+    document.getElementById('imgSol').style.boxShadow = '0px 0px 10px var(--btnPort)';
+    document.getElementById('imgSol').style.opacity = '1';
+    document.getElementById('imgBri').style.opacity = '0.4';
+    document.getElementById('imgBri').style.boxShadow = 'none';
+    document.getElementById('imgFias').style.opacity = '0.4';
+    document.getElementById('imgFias').style.boxShadow = 'none';
+}

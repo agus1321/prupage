2795f0913c9d55facf992bd5ebb45002549aa556
BrianSFarfan <brian.sfarfan@gmail.com>
2022-10-26 23:20:34 -0300

    PORTAFOLIO

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,16 @@
-function mostoFias(){
+function mostoFias() {
     document.getElementById('af').style.display = 'block';
     document.getElementById('bf').style.display = 'none';
     document.getElementById('ds').style.display = 'none';
 }
-function mostoBri(){
+
+function mostoBri() {
     document.getElementById('bf').style.display = 'block';
     document.getElementById('af').style.display = 'none';
     document.getElementById('ds').style.display = 'none';
 }
-function mostoSol(){
+
+function mostoSol() {
     document.getElementById('ds').style.display = 'block';
     document.getElementById('af').style.display = 'none';
     document.getElementById('bf').style.display = 'none';
@@ -19,29 +21,47 @@ function mostoSol(){
 
 
 
-function activarButtFias(){
+function activarButtFias() {
+    /*FIASCHE*/
     document.getElementById('imgFias').style.boxShadow = '0px 0px 10px var(--btnPort)';
     document.getElementById('imgFias').style.opacity = '1';
+    document.getElementById('imgFias').style.backgroundColor = 'var(--btnPort)';
+    /*BRIAN*/
     document.getElementById('imgBri').style.opacity = '0.4';
     document.getElementById('imgBri').style.boxShadow = 'none';
+    document.getElementById('imgBri').style.backgroundColor = 'rgba(34, 34, 34, 0.973)';
+    /*SOLER*/
     document.getElementById('imgSol').style.opacity = '0.4';
     document.getElementById('imgSol').style.boxShadow = 'none';
+    document.getElementById('imgSol').style.backgroundColor = 'rgba(34, 34, 34, 0.973)';
 }
 
-function activarButtBri(){
+function activarButtBri() {
+    /*BRIAN*/
     document.getElementById('imgBri').style.boxShadow = '0px 0px 10px var(--btnPort)';
     document.getElementById('imgBri').style.opacity = '1';
+    document.getElementById('imgBri').style.backgroundColor = 'var(--btnPort)';
+    /*SOLER*/
     document.getElementById('imgSol').style.opacity = '0.4';
     document.getElementById('imgSol').style.boxShadow = 'none';
+    document.getElementById('imgSol').style.backgroundColor = 'rgba(34, 34, 34, 0.973)';
+    /*FIASCHE*/
     document.getElementById('imgFias').style.opacity = '0.4';
     document.getElementById('imgFias').style.boxShadow = 'none';
+    document.getElementById('imgFias').style.backgroundColor = 'rgba(34, 34, 34, 0.973)';
 }
 
-function activarButtSol(){
+function activarButtSol() {
+    /*SOLER*/
     document.getElementById('imgSol').style.boxShadow = '0px 0px 10px var(--btnPort)';
     document.getElementById('imgSol').style.opacity = '1';
+    document.getElementById('imgSol').style.backgroundColor = 'var(--btnPort)';
+    /*BRIAN*/
     document.getElementById('imgBri').style.opacity = '0.4';
     document.getElementById('imgBri').style.boxShadow = 'none';
+    document.getElementById('imgBri').style.backgroundColor = 'rgba(34, 34, 34, 0.973)';
+    /*FIASCHE*/
     document.getElementById('imgFias').style.opacity = '0.4';
     document.getElementById('imgFias').style.boxShadow = 'none';
+    document.getElementById('imgFias').style.backgroundColor = 'rgba(34, 34, 34, 0.973)';
 }
